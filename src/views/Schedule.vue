d7660c6dc2c57f358733cc5997e68d9dfc8a5e04
Аlex Akhmetov <akhmetov@majordomo.ru>
2020-10-29 13:06:19 +0300

    pagination

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -2,14 +2,18 @@
   <div class="schedule">
     <div class="page-title">
       <h1>Расписание</h1>
-
-      <div class="schedule__list">
-        <b-list-group>
-          <b-list-group-item button v-for="(lesson, index) in lessons(current)" @click="goToLesson(lesson.id)" :key="index">{{ lesson.title }}</b-list-group-item>
-        </b-list-group>
-      </div>
-
     </div>
+
+    <div class="schedule__list">
+      <b-list-group>
+        <b-list-group-item button v-for="(lesson, index) in lessonsOnPage" @click="goToLesson(lesson.id)" :key="index">{{ lesson.title }}</b-list-group-item>
+      </b-list-group>
+    </div>
+
+    <div class="mt-3">
+      <b-pagination v-model="currentPage" @input="setPage(currentPage)" :total-rows="lessonsLength" :per-page="itemsPerPage" align="center"></b-pagination>
+    </div>
+
   </div>
 </template>
 
@@ -24,9 +28,26 @@ export default {
       lessons: 'lessonsByType',
       current: 'current',
     }),
+    lessonsOnPage() {
+      return this.lessons(this.current).slice(this.listFirstItem, this.listLastItem);
+    },
+    lessonsLength() {
+      return this.lessons(this.current).length;
+    },
+
   },
   methods: {
     ...mapActions(['fetchLessons']),
+
+    setPage(page) {
+      if (page === 1) {
+        this.listFirstItem = 0;
+        this.listLastItem = this.itemsPerPage;
+      } else {
+        this.listFirstItem = (page - 1) * this.itemsPerPage;
+        this.listLastItem = this.listFirstItem + this.itemsPerPage;
+      }
+    },
 
     goToLesson(id) {
       this.$router.push({ name: 'Lesson', params: { id } });
@@ -34,6 +55,14 @@ export default {
   },
   async mounted() {
     await this.fetchLessons()
+  },
+  data() {
+    return {
+      itemsPerPage: 5,
+      currentPage: 1,
+      listFirstItem: 0,
+      listLastItem: 5
+    }
   }
 }
 
